2ea3f75f573df2c5beba0981f4f6e0abce5d81f0
worstprgr <adam@seishin.io>
2024-07-09 23:09:21 +0200

    Added -d arg

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # IP Adapt
-This bash script replaces a **single** IP in the `allow <IP>` line of a nginx config.  
+This bash script replaces a **single** IP in the `allow <IP>` line of a nginx config,
+and reloads nginx after that.  
 The intention was, to whitelist quick and easy my IP for a specific http route.  
 
-*Note:* this script performs a backup of the target file, via `sed -i.bak`.  
+## Usage
+`./ipadapt.sh [-d]`  
 
-## bashrc
-I added for convenience an alias to my .bashrc, so it reloads nginx after the replace.  
-`alias ipadapt='sudo /YourPath>/ipadapt.sh && sudo nginx -s reload'`  
+If the argument `-d` is passed, it will comment the `allow <IP>` line.  
+If the argument is omitted, it will replace the old IP, with your current IP,
+and it will remove the comment.  
+
+The idea behind that argument is, that it can "remove" the `allow` entry via a cronjob.  
+
+## Backup
+Before any editing, it will perform a backup of the file, as `<file>.bak`.  
 
--- ipadapt.sh
+++ ipadapt.sh
@@ -1,26 +1,63 @@
 #!/bin/bash
 
+# Important: This script is intented to modify ONE 'allow' entry
+
+first_arg=$1
+
+if [ "$1" = "-h" ]; then
+    echo "$name Pass nothing, to adapt the ip. Pass '-d' to remove the 'allow' entry."
+    exit 0
+fi
+
 file="/etc/nginx/conf.d/www.your-page.foo.conf"
 regexp='[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}'
 regexp_sed='[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}'
 
 name="[IP Adapt]:"
 
-echo "$name Start ..."
+echo "${name} Start ..."
 
 new_ip=$(w | grep -Eo $regexp | head -n 1)
 
+
 if [ ! -f "$file" ]; then
-        echo "$name File '$file' does not exist"
+        echo "${name} Error: File '$file' does not exist"
         exit 1
 fi
+
 
 if [ -z "$new_ip" ]; then
-        echo "$name No IP found"
+        echo "${name} Error: No IP found"
         exit 1
 fi
 
-sed -i.bak "s/allow ${regexp_sed}/allow ${new_ip}/g" "$file"
+
+cp "$file" "${file}.bak"
+
+
+if [ "$1" = "-d" ]; then
+    if ! grep -q "allow" "$file"; then
+        echo "${name} No 'allow' found in file. Exiting"
+        exit 0
+    fi
+    if grep -q '^#.*allow' "$file"; then
+        echo "${name} Comment already present. Exiting"
+        exit 0
+    fi
+    echo "${name} Comment the 'allow' line"
+    sed -i '/allow/ s/^/#/' "$file"
+else
+    if grep -q '^#.*allow' "$file"; then
+        sed -i '/^#.*allow/ s/^#//' "$file"
+    fi
+    echo "${name} Replacing ip with: ${new_ip}"
+    sed -i "s/allow ${regexp_sed}/allow ${new_ip}/g" "$file"
+fi
+
+
+echo "${name} Reloading nginx ..."
 nginx -s reload
-echo "$name Terminated"
+
+echo "${name} Terminated"
 exit 0
+
